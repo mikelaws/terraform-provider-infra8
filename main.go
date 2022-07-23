42ab848e6db70ce7edff8adda151531041015ff9
Michael Laws <michael.laws@8x8.com>
2022-07-23 00:27:59 -0700

    Minor tweaks for flags and log

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/mikelaws/terraform-provider-infra8/cloudforms"
 )
@@ -10,7 +11,14 @@ func main() {
 	debugFlag := flag.Bool("debug", false, "Start provider in debug mode.")
 	flag.Parse()
 
-	plugin.Serve(&plugin.ServeOpts{
-		//Call provider
+	logFlags := log.Flags()
+	logFlags = logFlags &^ (log.Ldate | log.Ltime)
+	log.SetFlags(logFlags)
+
+	err = plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: cloudforms.Provider})
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
