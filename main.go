08b0c2ec7dd918f93bead30f66ab14405073c970
Michael Laws <michael.laws@8x8.com>
2022-07-23 00:29:40 -0700

    Removed flags and log for now

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/mikelaws/terraform-provider-infra8/cloudforms"
 )
 
 func main() {
-	debugFlag := flag.Bool("debug", false, "Start provider in debug mode.")
-	flag.Parse()
-
-	logFlags := log.Flags()
-	logFlags = logFlags &^ (log.Ldate | log.Ltime)
-	log.SetFlags(logFlags)
-
-	err = plugin.Serve(&plugin.ServeOpts{
+	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: cloudforms.Provider})
-
-	if err != nil {
-		log.Fatal(err)
-	}
 }
